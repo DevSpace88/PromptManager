67ec5640d4f67fa4ff5db6ee49f88dc33c7b59b6
Sergej Gorochow <sgorochow@hotmail.de>
2025-06-25 00:45:28 +0200

    http-force trick

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if (config('app.force_https', false)) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
